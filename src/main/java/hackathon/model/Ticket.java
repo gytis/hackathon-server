3c8c710f3617d30e22f9bfb7a6bcf994534f9335
gytis <gytis@redhat.com>
2015-06-14 13:16:36 +0100

    ss

--- src/main/java/hackathon/model/Ticket.java
+++ src/main/java/hackathon/model/Ticket.java
@@ -2,6 +2,7 @@ package hackathon.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -25,7 +26,7 @@ public class Ticket {
     @GeneratedValue(strategy = GenerationType.TABLE)
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "user_id")
     private User user;
 
